6e52abe06164ba11a49ee7f1b7ea6efd12ed12f6
antoniolucas777 <aanjos232@gmail.com>
2024-12-03 23:42:16 -0300

    main

--- src/index.js
+++ src/index.js
@@ -1,2 +1,27 @@
-let nome = "lucas";
-console.log(nome);
+console; log("Desafio classificador de nível de Herói");
+
+// Nome do herói , Nivel e quantidade de XP
+
+let nomeHeroi = "Ironman";
+let xP = ""
+let nivelHeroi = ""
+
+
+
+if (xP <= 1000) {
+    console.log("Ferro")
+}else if(Xp >= 1001 && xP <=2000){
+    console.log("Bronze")
+}else if(xp >=2001 && xp <= 5000){
+    console.log("Prata")
+}else if(xP >= 5001 && xP <= 7000){
+    console.log("Ouro")
+}else if(xP >= 7001 && xP <= 8000){
+    console.log("Platina")
+}else if (xP >= 8001 && xP <= 9000){
+    console.log("Ascendente")
+}else if(xP>= 9001 && xp <= 10000 ){
+    console.log("Imostal")
+}else if(xp >= 10001){ 
+    console.log("Radiante")
+}
